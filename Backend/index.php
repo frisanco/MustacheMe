72c258c7652d12da87344df63b2466e53c0b6134
Ben Frisanco <frisanco@msu.edu>
2018-04-21 02:33:18 -0400

    php test

--- Backend/index.php
+++ Backend/index.php
@@ -1,7 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Ben
- * Date: 4/21/2018
- * Time: 2:18 AM
- */
+echo "Hello World!";
+?>
+
+</body>
+</html>
